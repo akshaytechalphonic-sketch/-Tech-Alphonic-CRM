771c659967e84e2cfa59d0a2a2b4589ca024fb0a
Akshay kumar <akshaytechalphonic@gmail.com>
2025-12-30 15:00:52 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\ArtisanTerminalController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+////hello 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
