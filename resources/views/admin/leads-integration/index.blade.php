9e9ec42ab10439f4ad196335150770fb06711c18
Akshay kumar <akshaytechalphonic@gmail.com>
2026-01-30 15:23:15 +0530

    dsd

--- resources/views/admin/leads-integration/index.blade.php
+++ resources/views/admin/leads-integration/index.blade.php
@@ -301,9 +301,11 @@ async function getFacebookPagesFormLeads(form_id, access_token) {
     $('#facebookLoginButton').click(function() {
         console.log('click')
         // const facebookLoginUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${fbAppId}&redirect_uri=${redirectUri}&scope=pages_show_list,pages_read_engagement,leads_retrieval,pages_manage_ads`;
-        const facebookLoginUrl = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${fbAppId}&redirect_uri=${redirectUri}&scope=pages_show_list,pages_read_engagement,leads_retrieval,ads_management`;
-
-        
+       const facebookLoginUrl =
+`https://www.facebook.com/v19.0/dialog/oauth?
+client_id=${fbAppId}
+&redirect_uri=${redirectUri}
+&scope=pages_show_list,pages_read_engagement,leads_retrieval,ads_management`;
         const popup = window.open(facebookLoginUrl, 'facebookLoginPopup', 'width=600,height=700');
 
         $(window).on('message', function(event) {
