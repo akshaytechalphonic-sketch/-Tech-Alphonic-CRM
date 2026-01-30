532d5ca74acbfa5b0574b4e00a3fd0afcbf7142e
Akshay kumar <akshaytechalphonic@gmail.com>
2026-01-30 13:10:59 +0530

    changes

--- resources/views/admin/web.php
+++ resources/views/admin/web.php
@@ -144,13 +144,10 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
 
 
-    // Route::any('privacy-policy', [dashboardController::class, 'privacyPolicy'])->name('privacy-policy');
+    Route::any('privacy-policy', [dashboardController::class, 'privacyPolicy'])->name('privacy-policy');
 
 
 
     Route::any('share-you-travel', [dashboardController::class, 'shareYouTravel'])->name('share-you-travel');
     Route::any('delete-share-you-travel/{id}', [dashboardController::class, 'deleteshareYouTravel'])->name('delete-share-you-travel');
 });
-
-    Route::get('/privacy-policy', [DashboardController::class, 'privacyPolicy']);
-
