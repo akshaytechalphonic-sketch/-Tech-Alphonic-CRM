763c4413116f4139c1721aa58ebbc04f7910ab1c
Akshay kumar <akshaytechalphonic@gmail.com>
2026-01-30 16:23:26 +0530

    dsd

--- resources/views/admin/leads-integration/index.blade.php
+++ resources/views/admin/leads-integration/index.blade.php
@@ -300,14 +300,14 @@ async function getFacebookPagesFormLeads(form_id, access_token) {
 
     $('#facebookLoginButton').click(function() {
         console.log('click')
-        // const facebookLoginUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${fbAppId}&redirect_uri=${redirectUri}&scope=pages_show_list,pages_read_engagement,leads_retrieval,pages_manage_ads`;
-        const facebookLoginUrl =
-        `https://www.facebook.com/v19.0/dialog/oauth?client_id=${fbAppId}` +
-        `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-        `&scope=pages_show_list,leads_retrieval`;
-        
-        // window.location.href = facebookLoginUrl;
+        const facebookLoginUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${fbAppId}&redirect_uri=${redirectUri}&scope=pages_show_list,pages_read_engagement,leads_retrieval,pages_manage_ads`;
         const popup = window.open(facebookLoginUrl, 'facebookLoginPopup', 'width=600,height=700');
+//     const facebookLoginUrl =
+//   `https://www.facebook.com/v19.0/dialog/oauth?client_id=${fbAppId}` +
+//   `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+//   `&scope=pages_show_list,leads_retrieval`;
+
+// window.location.href = facebookLoginUrl;
         
 
         $(window).on('message', function(event) {
